5c544eebdf3f693b4c6158f9378282d0e8bc1ad0
SvenWarnke <sven@ge-warnke.de>
2024-04-09 21:18:53 +0200

    Update test_parquet_file_handling.py

--- tests/test_parquet_file_handling.py
+++ tests/test_parquet_file_handling.py
@@ -1,6 +1,6 @@
 import parquet_file_handling
 import pytest
-
+import pandas as pd
 
 
 def test_month_identifier():
@@ -13,3 +13,21 @@ def test_month_identifier():
     assert parquet_file_handling.MonthIdentifier(2020, 1) == parquet_file_handling.MonthIdentifier(2020, 1)
     assert parquet_file_handling.MonthIdentifier(2020, 2) > parquet_file_handling.MonthIdentifier(2020, 1)
 
+
+class TestMonthIdentifier:
+    @pytest.mark.parametrize('year, month, expected', [
+        (2020, 1, pd.Timestamp('2020-01', tz='UTC')),
+        (2020, 12, pd.Timestamp('2020-12', tz='UTC')),
+    ])
+    def test_start_timestamp(self, year, month, expected):
+        month_id = parquet_file_handling.MonthIdentifier(year, month)
+        assert month_id.start_timestamp() == expected
+
+    @pytest.mark.parametrize('year, month, exclusive_end', [
+        (2020, 1, pd.Timestamp('2020-02-01', tz='UTC')),
+        (2020, 12, pd.Timestamp('2021-01-01', tz='UTC')),
+    ])
+    def test_end_timestamp(self, year, month, exclusive_end):
+        month_id = parquet_file_handling.MonthIdentifier(year, month)
+        assert month_id.end_timestamp() < exclusive_end
+        assert month_id.end_timestamp() - exclusive_end < pd.Timedelta('1ms')
