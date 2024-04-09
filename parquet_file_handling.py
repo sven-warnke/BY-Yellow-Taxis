751ad61e7628e22377ece93e1c7169f947561004
SvenWarnke <sven@ge-warnke.de>
2024-04-09 18:04:56 +0200

    Update parquet_file_handling.py

--- parquet_file_handling.py
+++ parquet_file_handling.py
@@ -1,7 +1,7 @@
 import dataclasses
 import pathlib as pl
-import warnings
-from typing import List
+import logging
+from typing import List, Tuple
 
 import pandas as pd
 
@@ -23,6 +23,14 @@ class MonthIdentifier:
 
     def __le__(self, other: 'MonthIdentifier') -> bool:
         return (self.year, self.month) <= (other.year, other.month)
+
+    def to_tuple(self) -> Tuple[int, int]:
+        return self.year, self.month
+
+    def next_month(self) -> 'MonthIdentifier':
+        return MonthIdentifier(self.year + (self.month + 1) // 13, (self.month % 12) + 1)
+
+
 
 
 def parquet_file_name(month_id: MonthIdentifier) -> str:
@@ -48,14 +56,18 @@ def load_parquet_file(month_id: MonthIdentifier) -> pd.DataFrame:
 
 def filter_df_for_correct_time(df: pd.DataFrame, month_id: MonthIdentifier) -> pd.DataFrame:
     time_period = pd.Period(year=month_id.year, month=month_id.month, freq='M')
+    buffer_slight_overlap = pd.Timedelta(1, unit='hour')  # parquet files sometimes include a few entries from the next or previous month
 
-    invalid_indices = (df[DEFINING_TIME_COLUMN] < time_period.start_time) | (
-            df[DEFINING_TIME_COLUMN] >= time_period.end_time)
-    if invalid_indices.any():
-        warnings.warn(
-            f'Found {invalid_indices.sum()} entries with invalid time in {month_id.year}-{month_id.month:02}. They will be removed.')
+    start_limit = time_period.start_time - buffer_slight_overlap
+    end_limit = time_period.end_time + buffer_slight_overlap
 
-    return df[~invalid_indices]
+    out_of_range_indices = (df[DEFINING_TIME_COLUMN] < start_limit) | (
+            df[DEFINING_TIME_COLUMN] >= end_limit)
+    if out_of_range_indices.any():
+        logging.warning(
+            f'Found {out_of_range_indices.sum()} entries with invalid time in {month_id.year}-{month_id.month:02}. They will be removed.')
+
+    return df[~out_of_range_indices]
 
 
 def load_filtered_parquet_file(month_id: MonthIdentifier) -> pd.DataFrame:
@@ -65,7 +77,9 @@ def load_filtered_parquet_file(month_id: MonthIdentifier) -> pd.DataFrame:
 
 def daily_means_from_df(df: pd.DataFrame) -> pd.DataFrame:
     df['date'] = df['tpep_pickup_datetime'].dt.date
-    return df.groupby('date', as_index=False)[['trip_distance', 'trip_length_time']].mean()
+
+    # also collect count to be able to combine dataframes later
+    return df.groupby('date')[['trip_distance', 'trip_length_time']].agg(['mean', 'count'])
 
 
 def get_daily_means_for_month(month_id: MonthIdentifier) -> pd.DataFrame:
@@ -81,7 +95,7 @@ def get_months_in_range_inclusive(start: MonthIdentifier, end: MonthIdentifier) 
     current = start
     while current <= end:
         months.append(current)
-        current = MonthIdentifier(current.year + (current.month + 1) // 13, (current.month % 12) + 1)
+        current = current.next_month()
 
     return months
 
@@ -89,10 +103,10 @@ def get_months_in_range_inclusive(start: MonthIdentifier, end: MonthIdentifier) 
 def get_daily_means_in_range(start: MonthIdentifier, end: MonthIdentifier) -> pd.DataFrame:
     months = get_months_in_range_inclusive(start, end)
 
-    daily_means = [
-        get_daily_means_for_month(month_id)
+    daily_means = {
+        month_id.to_tuple(): get_daily_means_for_month(month_id)
         for month_id in months
-    ]
+    }
 
     daily_means_df = pd.concat(daily_means)
     return daily_means_df
