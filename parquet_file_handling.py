ed644d238b56a8f5098885a1423a8460c945ad66
SvenWarnke <sven@ge-warnke.de>
2024-04-10 10:37:24 +0200

    Update parquet_file_handling.py

--- parquet_file_handling.py
+++ parquet_file_handling.py
@@ -264,12 +264,24 @@ def get_daily_means_in_range(start: MonthIdentifier, end: MonthIdentifier) -> pd
     return daily_means_df
 
 
-def get_45day_rolling_mean(daily_means_df: pd.DataFrame) -> pd.DataFrame:
+def _prepare_df_for_grouping_operations(daily_means_df: pd.DataFrame) -> pd.DataFrame:
     if daily_means_df.index.name != 'date':
         raise ValueError('Index must be date')
     daily_means_df = daily_means_df.sort_index().reset_index()
     daily_means_df['date'] = pd.to_datetime(daily_means_df['date'])
     daily_means_df['trip_length_in_mins'] = daily_means_df['trip_length_time'].dt.total_seconds() / 60
-    daily_means_df[['roll_trip_distance', 'roll_trip_length_in_mins']] = daily_means_df.rolling('45D', on='date')[
-        'trip_distance', 'trip_length_in_mins'].mean()
     return daily_means_df
+
+
+def get_45day_rolling_mean(daily_means_df: pd.DataFrame) -> pd.DataFrame:
+    daily_means_df = _prepare_df_for_grouping_operations(daily_means_df)
+    daily_means_df[['roll_trip_distance', 'roll_trip_length_in_mins']] = daily_means_df.rolling('45D', on='date')[[
+        'trip_distance', 'trip_length_in_mins']].mean()
+    return daily_means_df
+
+
+def get_monthly_means(daily_means_df: pd.DataFrame) -> pd.DataFrame:
+    daily_means_df = _prepare_df_for_grouping_operations(daily_means_df)
+    monthly_means = daily_means_df.groupby(pd.Grouper(key='date', freq='M'))[[
+        'trip_distance', 'trip_length_in_mins']].mean()
+    return monthly_means
