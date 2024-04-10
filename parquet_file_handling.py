58c0a13be9239fa21f69a2d00329edfc6f6cb9ad
SvenWarnke <sven@ge-warnke.de>
2024-04-10 10:44:31 +0200

    ruff

--- parquet_file_handling.py
+++ parquet_file_handling.py
@@ -8,13 +8,13 @@ from typing import List, Tuple, Dict
 import pandas as pd
 import pyarrow.parquet
 
-DATA_FOLDER = pl.Path(__file__).parent / 'data'
+DATA_FOLDER = pl.Path(__file__).parent / "data"
 
-TIME_COLUMNS = ['tpep_pickup_datetime', 'tpep_dropoff_datetime']
-DEFINING_TIME_COLUMN = 'tpep_pickup_datetime'
+TIME_COLUMNS = ["tpep_pickup_datetime", "tpep_dropoff_datetime"]
+DEFINING_TIME_COLUMN = "tpep_pickup_datetime"
 
-ASSUMED_ORIGIN_TZ = 'UTC'
-NEW_YORK_TZ = 'US/Eastern'
+ASSUMED_ORIGIN_TZ = "UTC"
+NEW_YORK_TZ = "US/Eastern"
 
 
 @dataclasses.dataclass
@@ -24,48 +24,58 @@ class MonthIdentifier:
 
     def __post_init__(self):
         if self.month < 1 or self.month > 12:
-            raise ValueError(f'Month must be between 1 and 12, got {self.month}')
+            raise ValueError(f"Month must be between 1 and 12, got {self.month}")
 
-    def __lt__(self, other: 'MonthIdentifier') -> bool:
+    def __lt__(self, other: "MonthIdentifier") -> bool:
         return (self.year, self.month) < (other.year, other.month)
 
-    def __le__(self, other: 'MonthIdentifier') -> bool:
+    def __le__(self, other: "MonthIdentifier") -> bool:
         return (self.year, self.month) <= (other.year, other.month)
 
-    def next_month(self) -> 'MonthIdentifier':
-        return MonthIdentifier(self.year + (self.month + 1) // 13, (self.month % 12) + 1)
+    def next_month(self) -> "MonthIdentifier":
+        return MonthIdentifier(
+            self.year + (self.month + 1) // 13, (self.month % 12) + 1
+        )
 
     def start_timestamp(self) -> pd.Timestamp:
-        return pd.Period(year=self.year, month=self.month, freq='M').start_time.tz_localize(ASSUMED_ORIGIN_TZ)
+        return pd.Period(
+            year=self.year, month=self.month, freq="M"
+        ).start_time.tz_localize(ASSUMED_ORIGIN_TZ)
 
     def end_timestamp(self) -> pd.Timestamp:
-        return pd.Period(year=self.year, month=self.month, freq='M').end_time.tz_localize(ASSUMED_ORIGIN_TZ)
+        return pd.Period(
+            year=self.year, month=self.month, freq="M"
+        ).end_time.tz_localize(ASSUMED_ORIGIN_TZ)
 
     def first_day_of_month(self) -> datetime.date:
         return datetime.date(self.year, self.month, 1)
 
     def last_day_of_month(self) -> datetime.date:
         next_month = self.next_month()
-        return datetime.date(next_month.year, next_month.month, 1) - datetime.timedelta(days=1)
+        return datetime.date(next_month.year, next_month.month, 1) - datetime.timedelta(
+            days=1
+        )
 
 
 def parquet_file_name(month_id: MonthIdentifier) -> str:
-    return f'yellow_tripdata_{month_id.year}-{month_id.month:02}.parquet'
+    return f"yellow_tripdata_{month_id.year}-{month_id.month:02}.parquet"
 
 
 def acquire_parquet_file(month_id: MonthIdentifier) -> pl.Path:
-    url = f'https://d37ci6vzurychx.cloudfront.net/trip-data/{parquet_file_name(month_id)}'
+    url = (
+        f"https://d37ci6vzurychx.cloudfront.net/trip-data/{parquet_file_name(month_id)}"
+    )
     target = DATA_FOLDER / parquet_file_name(month_id)
     target.parent.mkdir(parents=True, exist_ok=True)
 
     if target.exists():
-        logging.info(f'File {target} already exists, skipping download')
+        logging.info(f"File {target} already exists, skipping download")
         return target
 
-    logging.info(f'Downloading {url} to {target}')
+    logging.info(f"Downloading {url} to {target}")
     urllib.request.urlretrieve(url, filename=target)
     if not target.exists():
-        raise ValueError(f'Failed to download {url} to {target}')
+        raise ValueError(f"Failed to download {url} to {target}")
     return target
 
 
@@ -77,21 +87,25 @@ class ColumnMapping:
 
     def __post_init__(self):
         if self.pickup_time == self.dropoff_time:
-            raise ValueError('Pickup and dropoff time must be different')
+            raise ValueError("Pickup and dropoff time must be different")
         if self.pickup_time == self.distance:
-            raise ValueError('Pickup time and distance must be different')
+            raise ValueError("Pickup time and distance must be different")
         if self.dropoff_time == self.distance:
-            raise ValueError('Dropoff time and distance must be different')
+            raise ValueError("Dropoff time and distance must be different")
 
     def mapping_dict(self) -> Dict[str, str]:
         return {
-            self.pickup_time: 'tpep_pickup_datetime',
-            self.dropoff_time: 'tpep_dropoff_datetime',
-            self.distance: 'trip_distance'
+            self.pickup_time: "tpep_pickup_datetime",
+            self.dropoff_time: "tpep_dropoff_datetime",
+            self.distance: "trip_distance",
         }
 
     def matches_columns(self, columns: List[str]) -> bool:
-        return self.pickup_time in columns and self.dropoff_time in columns and self.distance in columns
+        return (
+            self.pickup_time in columns
+            and self.dropoff_time in columns
+            and self.distance in columns
+        )
 
     def column_names(self) -> List[str]:
         return [self.pickup_time, self.dropoff_time, self.distance]
@@ -100,16 +114,18 @@ class ColumnMapping:
 def get_column_mapping(parquet_file: pl.Path) -> ColumnMapping:
     column_names = pyarrow.parquet.ParquetFile(parquet_file).schema.names
     possible_schemas = [
-        ColumnMapping('tpep_pickup_datetime', 'tpep_dropoff_datetime', 'trip_distance'),
-        ColumnMapping('Trip_Pickup_DateTime', 'Trip_Dropoff_DateTime', 'Trip_Distance'),
-        ColumnMapping('pickup_datetime', 'dropoff_datetime', 'trip_distance'),
+        ColumnMapping("tpep_pickup_datetime", "tpep_dropoff_datetime", "trip_distance"),
+        ColumnMapping("Trip_Pickup_DateTime", "Trip_Dropoff_DateTime", "Trip_Distance"),
+        ColumnMapping("pickup_datetime", "dropoff_datetime", "trip_distance"),
     ]
 
     for schema in possible_schemas:
         if schema.matches_columns(column_names):
             return schema
 
-    raise ValueError(f'Could not find a matching schema for columns {column_names} in {parquet_file}')
+    raise ValueError(
+        f"Could not find a matching schema for columns {column_names} in {parquet_file}"
+    )
 
 
 def read_parquet_file_with_different_schema(parquet_file: pl.Path) -> pd.DataFrame:
@@ -128,32 +144,39 @@ def load_parquet_file(month_id: MonthIdentifier) -> pd.DataFrame:
 
     if df.isnull().any().any():
         # haven't found Nan values in the data yet, but I want to know if they appear
-        raise ValueError('Found NaN values in the loaded DataFrame')
+        raise ValueError("Found NaN values in the loaded DataFrame")
 
     for time_column in TIME_COLUMNS:
         if time_column not in df.columns:
-            raise ValueError(f'Column {time_column} not found in the loaded DataFrame')
-        df[time_column] = pd.to_datetime(df[time_column]).dt.tz_localize(ASSUMED_ORIGIN_TZ)
+            raise ValueError(f"Column {time_column} not found in the loaded DataFrame")
+        df[time_column] = pd.to_datetime(df[time_column]).dt.tz_localize(
+            ASSUMED_ORIGIN_TZ
+        )
 
-    df['trip_length_time'] = df['tpep_dropoff_datetime'] - df['tpep_pickup_datetime']
+    df["trip_length_time"] = df["tpep_dropoff_datetime"] - df["tpep_pickup_datetime"]
 
     return df
 
 
-def filter_df_for_correct_time(df: pd.DataFrame, month_id: MonthIdentifier) -> pd.DataFrame:
-    buffer_slight_overlap = pd.Timedelta(1,
-                                         unit='hour')  # parquet files sometimes include a few entries from the next or previous month
+def filter_df_for_correct_time(
+    df: pd.DataFrame, month_id: MonthIdentifier
+) -> pd.DataFrame:
+    buffer_slight_overlap = pd.Timedelta(
+        1, unit="hour"
+    )  # parquet files sometimes include a few entries from the next or previous month
 
     start_limit = month_id.start_timestamp() - buffer_slight_overlap
     end_limit = month_id.end_timestamp() + buffer_slight_overlap
 
     out_of_range_indices = (df[DEFINING_TIME_COLUMN] < start_limit) | (
-            df[DEFINING_TIME_COLUMN] >= end_limit)
+        df[DEFINING_TIME_COLUMN] >= end_limit
+    )
     if out_of_range_indices.any():
         logging.warning(
-            f'Found {out_of_range_indices.sum()} entries with invalid time in {month_id.year}-{month_id.month:02}. They will be removed.')
+            f"Found {out_of_range_indices.sum()} entries with invalid time in {month_id.year}-{month_id.month:02}. They will be removed."
+        )
         for i, row in df[out_of_range_indices].iterrows():
-            logging.info(f'Invalid time at index {i}: {row[DEFINING_TIME_COLUMN]}')
+            logging.info(f"Invalid time at index {i}: {row[DEFINING_TIME_COLUMN]}")
 
     return df[~out_of_range_indices]
 
@@ -164,13 +187,13 @@ def load_filtered_parquet_file(month_id: MonthIdentifier) -> pd.DataFrame:
 
 
 def daily_means_from_df(df: pd.DataFrame) -> pd.DataFrame:
-    df['date'] = df['tpep_pickup_datetime'].dt.date
+    df["date"] = df["tpep_pickup_datetime"].dt.date
 
     # also collect count to be able to combine dataframes later
-    return df.groupby('date').agg(
-        trip_distance=('trip_distance', 'mean'),
-        trip_length_time=('trip_length_time', 'mean'),
-        count=('trip_distance', 'count')
+    return df.groupby("date").agg(
+        trip_distance=("trip_distance", "mean"),
+        trip_length_time=("trip_length_time", "mean"),
+        count=("trip_distance", "count"),
     )
 
 
@@ -179,9 +202,11 @@ def get_daily_means_for_month(month_id: MonthIdentifier) -> pd.DataFrame:
     return daily_means_from_df(filtered_df)
 
 
-def get_months_in_range_inclusive(start: MonthIdentifier, end: MonthIdentifier) -> List[MonthIdentifier]:
+def get_months_in_range_inclusive(
+    start: MonthIdentifier, end: MonthIdentifier
+) -> List[MonthIdentifier]:
     if start > end:
-        raise ValueError('Start month must be before end month')
+        raise ValueError("Start month must be before end month")
 
     months = []
     current = start
@@ -194,20 +219,27 @@ def get_months_in_range_inclusive(start: MonthIdentifier, end: MonthIdentifier) 
 
 def combine_rows_via_weighted_average(row1: pd.Series, row2: pd.Series) -> pd.Series:
     if row1.name != row2.name:
-        raise ValueError('Rows must have the same index')
+        raise ValueError("Rows must have the same index")
 
-    return pd.Series({
-        'trip_distance': row1['trip_distance'] * row1['count'] + row2['trip_distance'] * row2['count'],
-        'trip_length_time': row1['trip_length_time'] * row1['count'] + row2['trip_length_time'] * row2['count'],
-        'count': row1['count'] + row2['count']
-    }) / (row1['count'] + row2['count'])
+    return pd.Series(
+        {
+            "trip_distance": row1["trip_distance"] * row1["count"]
+            + row2["trip_distance"] * row2["count"],
+            "trip_length_time": row1["trip_length_time"] * row1["count"]
+            + row2["trip_length_time"] * row2["count"],
+            "count": row1["count"] + row2["count"],
+        }
+    ) / (row1["count"] + row2["count"])
 
 
-def fix_first_and_last_days_of_consecutive_dfs(df_before: pd.DataFrame, month_before: MonthIdentifier,
-                                               df_after: pd.DataFrame,
-                                               month_after: MonthIdentifier) -> Tuple[pd.DataFrame, pd.DataFrame]:
+def fix_first_and_last_days_of_consecutive_dfs(
+    df_before: pd.DataFrame,
+    month_before: MonthIdentifier,
+    df_after: pd.DataFrame,
+    month_after: MonthIdentifier,
+) -> Tuple[pd.DataFrame, pd.DataFrame]:
     if month_before.next_month() != month_after:
-        raise ValueError('Months must be consecutive')
+        raise ValueError("Months must be consecutive")
 
     if df_before.empty or df_after.empty:
         return df_before, df_after
@@ -216,21 +248,26 @@ def fix_first_and_last_days_of_consecutive_dfs(df_before: pd.DataFrame, month_be
     if potentially_overlapping_date in df_before.index:
         print("Fixing before", df_before.loc[potentially_overlapping_date])
         df_after.loc[potentially_overlapping_date] = combine_rows_via_weighted_average(
-            df_before.loc[potentially_overlapping_date], df_after.loc[potentially_overlapping_date])
+            df_before.loc[potentially_overlapping_date],
+            df_after.loc[potentially_overlapping_date],
+        )
         df_before = df_before.drop(potentially_overlapping_date)
 
     potentially_overlapping_date = month_before.last_day_of_month()
     if potentially_overlapping_date in df_after.index:
         print("Fixing after", df_after.loc[potentially_overlapping_date])
         df_before.loc[potentially_overlapping_date] = combine_rows_via_weighted_average(
-            df_before.loc[potentially_overlapping_date], df_after.loc[potentially_overlapping_date])
+            df_before.loc[potentially_overlapping_date],
+            df_after.loc[potentially_overlapping_date],
+        )
         df_after = df_after.drop(potentially_overlapping_date)
 
     return df_before, df_after
 
 
-def fix_first_and_last_days_in_list_of_dfs(month_daily_means_tuples: List[Tuple[MonthIdentifier, pd.DataFrame]]) -> \
-List[pd.DataFrame]:
+def fix_first_and_last_days_in_list_of_dfs(
+    month_daily_means_tuples: List[Tuple[MonthIdentifier, pd.DataFrame]],
+) -> List[pd.DataFrame]:
     if len(month_daily_means_tuples) == 1:
         return [month_daily_means_tuples[0][1]]
 
@@ -238,8 +275,11 @@ List[pd.DataFrame]:
     first_month, first_daily_means = month_daily_means_tuples[0]
 
     for second_month, second_daily_means in month_daily_means_tuples[1:]:
-        first_daily_means, second_daily_means = fix_first_and_last_days_of_consecutive_dfs(
-            first_daily_means, first_month, second_daily_means, second_month)
+        first_daily_means, second_daily_means = (
+            fix_first_and_last_days_of_consecutive_dfs(
+                first_daily_means, first_month, second_daily_means, second_month
+            )
+        )
         daily_means.append(first_daily_means)
         first_month = second_month
         first_daily_means = second_daily_means
@@ -248,40 +288,49 @@ List[pd.DataFrame]:
     return daily_means
 
 
-def get_daily_means_in_range(start: MonthIdentifier, end: MonthIdentifier) -> pd.DataFrame:
+def get_daily_means_in_range(
+    start: MonthIdentifier, end: MonthIdentifier
+) -> pd.DataFrame:
     months = get_months_in_range_inclusive(start, end)
 
     month_daily_means_tuples = [
-        (month_id, get_daily_means_for_month(month_id))
-        for month_id in months
+        (month_id, get_daily_means_for_month(month_id)) for month_id in months
     ]
 
     if not month_daily_means_tuples:
-        raise ValueError('No months found in range')
+        raise ValueError("No months found in range")
 
-    daily_means_df_list = fix_first_and_last_days_in_list_of_dfs(month_daily_means_tuples)
+    daily_means_df_list = fix_first_and_last_days_in_list_of_dfs(
+        month_daily_means_tuples
+    )
     daily_means_df = pd.concat(daily_means_df_list)
     return daily_means_df
 
 
 def _prepare_df_for_grouping_operations(daily_means_df: pd.DataFrame) -> pd.DataFrame:
-    if daily_means_df.index.name != 'date':
-        raise ValueError('Index must be date')
+    if daily_means_df.index.name != "date":
+        raise ValueError("Index must be date")
     daily_means_df = daily_means_df.sort_index().reset_index()
-    daily_means_df['date'] = pd.to_datetime(daily_means_df['date'])
-    daily_means_df['trip_length_in_mins'] = daily_means_df['trip_length_time'].dt.total_seconds() / 60
+    daily_means_df["date"] = pd.to_datetime(daily_means_df["date"])
+    daily_means_df["trip_length_in_mins"] = (
+        daily_means_df["trip_length_time"].dt.total_seconds() / 60
+    )
     return daily_means_df
 
 
 def get_45day_rolling_mean(daily_means_df: pd.DataFrame) -> pd.DataFrame:
     daily_means_df = _prepare_df_for_grouping_operations(daily_means_df)
-    daily_means_df[['roll_trip_distance', 'roll_trip_length_in_mins']] = daily_means_df.rolling('45D', on='date')[[
-        'trip_distance', 'trip_length_in_mins']].mean()
+    daily_means_df[["roll_trip_distance", "roll_trip_length_in_mins"]] = (
+        daily_means_df.rolling(
+            "45D", on="date"
+        )[["trip_distance", "trip_length_in_mins"]].mean()
+    )
     return daily_means_df
 
 
 def get_monthly_means(daily_means_df: pd.DataFrame) -> pd.DataFrame:
     daily_means_df = _prepare_df_for_grouping_operations(daily_means_df)
-    monthly_means = daily_means_df.groupby(pd.Grouper(key='date', freq='M'))[[
-        'trip_distance', 'trip_length_in_mins']].mean()
+    monthly_means = daily_means_df.groupby(pd.Grouper(key="date", freq="M"))[
+        ["trip_distance", "trip_length_in_mins"]
+    ].mean()
     return monthly_means
--- tests/test_parquet_file_handling.py
+++ tests/test_parquet_file_handling.py
@@ -4,42 +4,66 @@ import pandas as pd
 
 
 class TestMonthIdentifier:
-
-    @pytest.mark.parametrize('year, month', [
-        (2020, 13),
-        (2020, 0),
-    ])
+    @pytest.mark.parametrize(
+        "year, month",
+        [
+            (2020, 13),
+            (2020, 0),
+        ],
+    )
     def test_raises_on_invalid_input(self, year, month):
         with pytest.raises(ValueError):
             parquet_file_handling.MonthIdentifier(year, month)
 
     def test_lt(self):
-        assert parquet_file_handling.MonthIdentifier(2020, 1) < parquet_file_handling.MonthIdentifier(2020, 2)
-        assert not parquet_file_handling.MonthIdentifier(2020, 2) < parquet_file_handling.MonthIdentifier(2020, 1)
-        assert not parquet_file_handling.MonthIdentifier(2020, 1) < parquet_file_handling.MonthIdentifier(2020, 1)
+        assert parquet_file_handling.MonthIdentifier(
+            2020, 1
+        ) < parquet_file_handling.MonthIdentifier(2020, 2)
+        assert not parquet_file_handling.MonthIdentifier(
+            2020, 2
+        ) < parquet_file_handling.MonthIdentifier(2020, 1)
+        assert not parquet_file_handling.MonthIdentifier(
+            2020, 1
+        ) < parquet_file_handling.MonthIdentifier(2020, 1)
 
     def test_le(self):
-        assert parquet_file_handling.MonthIdentifier(2020, 1) <= parquet_file_handling.MonthIdentifier(2020, 2)
-        assert not parquet_file_handling.MonthIdentifier(2020, 2) <= parquet_file_handling.MonthIdentifier(2020, 1)
-        assert parquet_file_handling.MonthIdentifier(2020, 1) <= parquet_file_handling.MonthIdentifier(2020, 1)
+        assert parquet_file_handling.MonthIdentifier(
+            2020, 1
+        ) <= parquet_file_handling.MonthIdentifier(2020, 2)
+        assert not parquet_file_handling.MonthIdentifier(
+            2020, 2
+        ) <= parquet_file_handling.MonthIdentifier(2020, 1)
+        assert parquet_file_handling.MonthIdentifier(
+            2020, 1
+        ) <= parquet_file_handling.MonthIdentifier(2020, 1)
 
     def test_eq(self):
-        assert parquet_file_handling.MonthIdentifier(2020, 1) == parquet_file_handling.MonthIdentifier(2020, 1)
-        assert not parquet_file_handling.MonthIdentifier(2020, 1) == parquet_file_handling.MonthIdentifier(2020, 2)
+        assert parquet_file_handling.MonthIdentifier(
+            2020, 1
+        ) == parquet_file_handling.MonthIdentifier(2020, 1)
+        assert not parquet_file_handling.MonthIdentifier(
+            2020, 1
+        ) == parquet_file_handling.MonthIdentifier(2020, 2)
 
-    @pytest.mark.parametrize('year, month, expected', [
-        (2020, 1, pd.Timestamp('2020-01', tz='UTC')),
-        (2020, 12, pd.Timestamp('2020-12', tz='UTC')),
-    ])
+    @pytest.mark.parametrize(
+        "year, month, expected",
+        [
+            (2020, 1, pd.Timestamp("2020-01", tz="UTC")),
+            (2020, 12, pd.Timestamp("2020-12", tz="UTC")),
+        ],
+    )
     def test_start_timestamp(self, year, month, expected):
         month_id = parquet_file_handling.MonthIdentifier(year, month)
         assert month_id.start_timestamp() == expected
 
-    @pytest.mark.parametrize('year, month, exclusive_end', [
-        (2020, 1, pd.Timestamp('2020-02-01', tz='UTC')),
-        (2020, 12, pd.Timestamp('2021-01-01', tz='UTC')),
-    ])
+    @pytest.mark.parametrize(
+        "year, month, exclusive_end",
+        [
+            (2020, 1, pd.Timestamp("2020-02-01", tz="UTC")),
+            (2020, 12, pd.Timestamp("2021-01-01", tz="UTC")),
+        ],
+    )
     def test_end_timestamp(self, year, month, exclusive_end):
         month_id = parquet_file_handling.MonthIdentifier(year, month)
         assert month_id.end_timestamp() < exclusive_end
-        assert month_id.end_timestamp() - exclusive_end < pd.Timedelta('1ms')
+        assert month_id.end_timestamp() - exclusive_end < pd.Timedelta("1ms")
