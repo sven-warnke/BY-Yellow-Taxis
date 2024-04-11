f8fcd2acd018a0529bd926facb5c825c40c87135
SvenWarnke <sven@ge-warnke.de>
2024-04-11 15:55:59 +0200

    Update report.py

--- helpers/report.py
+++ helpers/report.py
@@ -1,17 +1,9 @@
 from typing import List
 
-import dominate
 from plotly import graph_objects as go
 
 
-def save_plots(figures: List[go.Figure], headers: List[str], filename: str) -> None:
-    if len(figures) != len(headers):
-        raise ValueError("Number of figures and headers must be the same")
-
-    doc = dominate.document(title="Report")
-    with doc:
-        for figure_name, figure in zip(headers, figures):
-            dominate.tags.h1(figure_name)
-            dominate.tags.div(figure.to_html(full_html=False, include_plotlyjs="cdn"))
+def save_plots(figures: List[go.Figure], filename: str) -> None:
     with open(filename, "w") as f:
-        f.write(doc.render())
+        for fig in figures:
+            f.write(fig.to_html(full_html=False, include_plotlyjs="cdn"))
