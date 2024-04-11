e8ccf36efd0288c1cfdf55903915a8b042e5c2f2
SvenWarnke <sven@ge-warnke.de>
2024-04-11 21:14:10 +0200

    Update test_parquet_file_handling.py

--- tests/test_parquet_file_handling.py
+++ tests/test_parquet_file_handling.py
@@ -67,3 +67,51 @@ class TestMonthIdentifier:
         month_id = parquet_file_handling.MonthIdentifier(year, month)
         assert month_id.end_timestamp() < exclusive_end
         assert month_id.end_timestamp() - exclusive_end < pd.Timedelta("1ms")
+
+
+def test_combine_dfs_via_weighted_average():
+    df1 = pd.DataFrame(
+        {
+            "date": ["2020-01-01", "2020-01-02", "2020-01-03"],
+            "trip_distance": [1, 2, 3],
+            "trip_length_time": [
+                pd.Timedelta("1 hour"),
+                pd.Timedelta("2 hours"),
+                pd.Timedelta("3 hours"),
+            ],
+            "count": [1, 1, 1],
+        }
+    )
+    df1 = df1.set_index("date")
+    df2 = pd.DataFrame(
+        {
+            "date": ["2020-01-01", "2020-01-02", "2020-01-03"],
+            "trip_distance": [2, 3, 4],
+            "trip_length_time": [
+                pd.Timedelta("2 hours"),
+                pd.Timedelta("3 hours"),
+                pd.Timedelta("4 hours"),
+            ],
+            "count": [2, 2, 2],
+        }
+    )
+    df2 = df2.set_index("date")
+    result = parquet_file_handling.combine_dfs_via_weighted_average(df1, df2)
+    expected = pd.DataFrame(
+        {
+            "trip_distance": [5 / 3, 8 / 3, 11 / 3],
+            "trip_length_time": [
+                pd.Timedelta(5 / 3, "hours"),
+                pd.Timedelta(8 / 3, "hours"),
+                pd.Timedelta(11 / 3, "hours"),
+            ],
+            "date": ["2020-01-01", "2020-01-02", "2020-01-03"],
+            "count": [3, 3, 3],
+        }
+    )
+    expected = expected.set_index("date")
+    expected["trip_length_time"] = expected[
+        "trip_length_time"
+    ].dt.total_seconds()  # convert to seconds because assert_frame_equal does not support Timedelta
+    result["trip_length_time"] = result["trip_length_time"].dt.total_seconds()
+    pd.testing.assert_frame_equal(result, expected, check_exact=False)
