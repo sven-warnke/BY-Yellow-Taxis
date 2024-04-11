2fc695d812fdf207710b997cd15c44c0d0e457f1
SvenWarnke <sven@ge-warnke.de>
2024-04-11 20:40:59 +0200

    Update report.py

--- helpers/report.py
+++ helpers/report.py
@@ -1,9 +1,11 @@
+import pathlib as pl
 from typing import List
 
 from plotly import graph_objects as go
 
 
-def save_plots(figures: List[go.Figure], filename: str) -> None:
+def save_plots(figures: List[go.Figure], filename: pl.Path) -> None:
+    filename.parent.mkdir(parents=True, exist_ok=True)
     with open(filename, "w") as f:
         for fig in figures:
             f.write(fig.to_html(full_html=False, include_plotlyjs="cdn"))
