c12826c37e92f2f6c580468cab924a3cb8e21065
SvenWarnke <sven@ge-warnke.de>
2024-04-11 15:03:22 +0200

    Update create_report.py

--- create_report.py
+++ create_report.py
@@ -1,6 +1,6 @@
 import argparse
 
-from helpers import parquet_file_handling, plotting
+from helpers import parquet_file_handling, plotting, report
 
 
 def main():
@@ -9,6 +9,7 @@ def main():
     parser.add_argument("--start-month", type=int, default=1)
     parser.add_argument("--end-year", type=int, default=2024)
     parser.add_argument("--end-month", type=int, default=1)
+    parser.add_argument("--output-path", type=str, default="monthly_mean.html")
     args = parser.parse_args()
 
     start_month_id = parquet_file_handling.MonthIdentifier(
@@ -18,7 +19,21 @@ def main():
     daily_means_df = parquet_file_handling.get_daily_means_in_range(
         start_month_id, end_month_id
     )
-    monthly_mean = plotting.time_wise_averaging.MonthlyMean()
+
     monthly_mean_fig = plotting.plot_rolling_means_for_time_and_distance(
-        daily_means_df, monthly_mean
+        daily_means_df, plotting.time_wise_averaging.MonthlyMean()
     )
+
+    rolling_mean_fig = plotting.plot_rolling_means_for_time_and_distance(
+        daily_means_df, plotting.time_wise_averaging.RollingMean()
+    )
+
+    report.save_plots(
+        figures=[monthly_mean_fig, rolling_mean_fig],
+        headers=["Monthly average", "Rolling average 45 days"],
+        filename=args.output_path,
+    )
+
+
+if __name__ == "__main__":
+    main()
